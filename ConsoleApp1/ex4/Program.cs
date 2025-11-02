747250521d53845d2be82422fd2dda35bfe06f29
david <zollner.david010306@gmail.com>
2025-11-03 00:17:21 +0100

    Implement bubble sort

---
+++ ConsoleApp1/ex4/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ex4;
+
+class Program
+{
+    private static int[] arr = new int[10];
+
+    private static void randomArray()
+    {
+        for (var i = 0; i < arr.Length; i++)
+        {
+            arr[i] = Random.Shared.Next(1000);
+        }
+    }
+
+    private static void bubbleSort()
+    {
+        for (int i = 0; i < arr.Length - 1; i++)
+        {
+            for (int j = 0; j < arr.Length - i - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    (arr[j], arr[j + 1]) = (arr[j + 1], arr[j]); // swap
+                }
+            }
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        randomArray();
+        bubbleSort();
+        Console.WriteLine(arr);
+    }
+}
